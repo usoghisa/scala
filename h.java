023fee1c279a0dd08f77a644cfdfc3b951fafe0c
usoghisa <usoghisa@hotmail.com>
2014-10-06 20:06:35 +0100

    Update h.java

--- h.java
+++ h.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class h
-// mod 2
+// mod 333333
 {
 		private static Scanner scanner=new Scanner(System.in); static{scanner.useDelimiter(System.getProperty("line.separator"));}
 	public static void main(String[] args)
