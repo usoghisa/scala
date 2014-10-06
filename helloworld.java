6b3b90f8bd6af2dc0fb3f1487c79f64f619246d1
unknown <upisa01@CSPC100.dcs.bbk.ac.uk>
2014-10-06 19:25:32 +0100

    uuuuuuu u

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class helloworld
+///////////////////uuu
+{
+		private static Scanner scanner=new Scanner(System.in); static{scanner.useDelimiter(System.getProperty("line.separator"));}
+	public static void main(String[] args)
+	{
+System.out.print("Hello ");
+System.out.println("World!");
+}}
+
