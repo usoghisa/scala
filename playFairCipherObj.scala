8f24b29b33b0eda34b5622a65ce5429085250b5c
usoghisa <usoghisa@hotmail.com>
2015-01-31 20:26:54 +0000

    Update playFairCipherObj.scala
    encode decode pair

--- playFairCipherObj.scala
+++ playFairCipherObj.scala
@@ -7,58 +7,157 @@ import java.util.Scanner
 import scala.io.Source
 import java.io._
 import java.lang.{ StringBuilder => JavaStringBuilder }
-
+import Array._
 object playFairCipherObj {
-  
-  private val maxKeyLeng = 12
+  private var validateKey: String = ""
+  private var cipher: String = ""
+  val sz = 4
+  private var cipherMatrix = ofDim[Char](sz + 1, sz + 1)
+  private val maxKeyLeng = 6
   private val minKeyLeng = 4
-  private val toEncodfilename = "src\\playFairCipherPack\\toEncod.txt"
-  private var toEncod: String = ""   
-   
+  private val fileNameToEncode = "src\\playFairCipherPack\\toEncod.txt"
+  private var toEncod: String = ""
+  val alphabet = "abcdefghiklmnopqrstuvwxyz"
+
   def main(arg: Array[String]) {
-    
+    //print("Choose 1 encode and ENTER\nChoose 2 decode and ENTER\nChoose 3 quit and ENTER \n")
+    // validInputChoice()
+  }
+// this copy to be cancel
+  private def mkCipher2() {
+    var cipherT = ("password" + alphabet).replaceAll("""\s+""", "").toList.distinct.mkString // make cipher
+    print("cipherT.size is " + cipherT.size); println("The cipherT string is " + cipherT.mkString); println("Matrix is ")
+    var c = 0
+    for (i <- 0 to sz) {
+      for (j <- 0 to sz) {
+        cipherMatrix(i)(j) = cipherT(c);
+        c = c + 1
+      }
+    }
+    // Print two dimensional array
+    for (i <- 0 to sz) {
+      for (j <- 0 to sz) {
+        print(cipherMatrix(i)(j) + " ");
+      }
+      println()
+    }// find letter in chifer
+
+  }
+ 
+  mkCipher2() /////////////////testin purpose only
+  
+//_____________________________________________________
+  var encPair =('x','y')
+  encPair = encodeDecodePairLet('m','b')
+  println("encode m,b will be "+ encPair._1 + "," + encPair._2  )
+  encPair = encodeDecodePairLet('n','d')
+  println("encode n,d will be "+ encPair._1 + "," + encPair._2  )
+//_____________________________________________________
+  
+   def encodeDecodePairLet(l1: Char,l2: Char):(Char,Char) = {    
+    var c1 = (-1,-1)
+    var c2 = (-1,-1)
+    var temp = (-1,-1)
+    var pair = (-1,-1,-1,-1)
+    if (findChar(l1) == (-1,-1)) {print("sorry")}else{  c1 = findChar(l1); //print("okk");
+    } 
+    if (findChar(l2) == (-1,-1)) {print("sorry")}else{ c2 = findChar(l2);//print("okk");  
+    } 
+    temp = (c1._1, c1._2)
+    c1=(c1._1,c2._2)
+    c2=(c2._1,temp._2)
+    pair=(c1._1,c1._2, c2._1,c2._2) 
+    return(cipherMatrix(pair._1)(pair._2), cipherMatrix(pair._3)(pair._4))
+  }
    
+  def findChar(p: Char): (Int,Int) = {
+   var ltrCoo=(-1,-1)//Tuple
+    for (i <- 0 to sz) {
+      for (j <- 0 to sz) {
+      if(cipherMatrix(i)(j)==p)  
+      {  ltrCoo = (i,j)
+    	  //println("Find "+p)
+      }else{(-1, -1)} 
+      }
+    }
+   //print(ltrCoo._1 +" "+ ltrCoo._2 ) // Tuple2 = (i,j)
+   return (ltrCoo._1,ltrCoo._2)
+  }
 
-    /* 
-    var toEncod2: String = "bnmbnm bnm sdfasf MMMM EEEE ,." 
-    toEncod2 = toEncod2.toLowerCase().replaceAll("[^a-z]", "");//no white space 
-     
-    // http://www.tutorialspoint.com/scala/scala_file_io.htm
-    val writer = new PrintWriter(new File("src\\playFairCipherPack\\test.txt"))
-    writer.write("Hello Scala")
-    writer.close()
-
-    println("Following is the content read:")
-    Source.fromFile("src\\playFairCipherPack\\test.txt").foreach {
-      print
-    }*/
-
-    print("Choose 1 encode and ENTER\nChoose 2 decode and ENTER\nChoose 3 quit and ENTER \n")
-    validInputChoice()
-
-
+  //////////////////////////////////
+  /**
+   * Formats the output message for display as specified in
+   * the assignment description.
+   *
+   * @param message The message to format.
+   * @param count The number of characters seen so far
+   * @return The message, in blocks of 5 characters separated by spaces, with newlines every 50 characters.
+   */
+  def fiveCharFormat(message: String, count: Int): String = {
+    if (message.length > 0) {
+      if (count % 50 == 0) {
+        message.charAt(0) + System.getProperty("line.separator") + fiveCharFormat(message.substring(1), count + 1)
+      } else if (count % 5 == 0) {
+        message.charAt(0) + " " + fiveCharFormat(message.substring(1), count + 1)
+      } else {
+        message.charAt(0) + fiveCharFormat(message.substring(1), count + 1)
+      }
+    } else {
+      message
+    }
   }
 
   def encode() {
-    validKeyword()
-    //
-    if (!(isTxtOk(toEncodfilename).equals("false"))) {
-      println("This is text for encode")
-      toEncod = (isTxtOk(toEncodfilename).replaceAll("\n", " ") )
-      toEncod = toEncod.toLowerCase().replaceAll("[^a-z ]", "")// white ok
-      println(toEncod);println(toEncod.replaceAll("[^a-z]", ""))
-    } else { println("Sorry") }
+    validateKeyword()
+    mkCipher()
+    println("This your valid KeyWord.." + validateKey)
+    if (!mkEncodeTxt().equals("FALSE")) print(fiveCharFormat(toEncod, toEncod.size)) else false
+  }
+
+  private def mkEncodeTxt(): String = {
+    if (!(isTxtOk(fileNameToEncode).equals("false"))) {
+      println("This is text for encode all j will be substiturte with i")
+      toEncod = (isTxtOk(fileNameToEncode).replaceAll("\n", " "))
+      toEncod = toEncod.toLowerCase().replaceAll("[^a-z ]", "") // preserve single whitespace
+      println(toEncod);
+      toEncod = toEncod.replaceAll("""\s+""", "") // white space yes ok
+      toEncod = toEncod.replaceAll("j", "i")
+      println(toEncod)
+      println(toEncod.replaceAll("[^a-z]", "")) // white space no
+      return toEncod
+    } else { println("Sorry Error Occured"); return "FALSE" }
+  }
+
+  private def mkCipher() {
+    cipher = (validateKey + alphabet).replaceAll("""\s+""", "").toList.distinct.mkString // make cifer
+    print("Cipher.size is " + cipher.size); println("The Cipher string is " + cipher.mkString); println("Matrix is ")
+    val sz = 4
+    var c = 0
+    var cipherMatrix = ofDim[Char](sz + 1, sz + 1)
+    for (i <- 0 to sz) {
+      for (j <- 0 to sz) {
+        cipherMatrix(i)(j) = cipher(c);
+        c = c + 1
+      }
+    }
+    // Print two dimensional array
+    for (i <- 0 to sz) {
+      for (j <- 0 to sz) {
+        print(cipherMatrix(i)(j) + " ");
+      }
+      println();
+    }
   }
 
   def decode() {
-    validKeyword()
+    validateKeyword()
     print("dencode ")
   }
 
   def isTxtOk(fileName: String): String = {
     var s = ""
     try {
-      for (line <- Source.fromFile(toEncodfilename).getLines) {
+      for (line <- Source.fromFile(fileNameToEncode).getLines) {
         s += line + "\n"
         // println(line)
       }
@@ -66,18 +165,23 @@ object playFairCipherObj {
       case e: FileNotFoundException => println("Couldn't find that file. I can not encode.")
       case e: IOException => println("Got an IOException! I can not encode.")
     }
-    if (s != "")  s else "false"
+    if (s != "") s else "false"
   }
 
-  def validKeyword() {
+  def validateKeyword() {
     var vld = false
     do {
-      val keyWord = readLine("Type your keyword between 4 and 12 character no number \n>")
-      def isOnlyChars(keyWord: String) = keyWord.matches("^[a-zA-Z]*$")
+      val keyWord = readLine("Type your keyword between 4 and 6 character \nNO NUMBER NO SPACE NO UPPERCASE NO DUPLICATE \nThe letter \"j\" will be substitute with  \"i\" \n>")
+      def isOnlyChars(keyWord: String) = keyWord.matches("^[a-z]*$")
       def isLessMaxKeyLeng(keyWord: String) = if (keyWord.length() <= maxKeyLeng) true else false
       def isOkMinKeyLeng(keyWord: String) = if (keyWord.length() >= minKeyLeng) true else false
-      if (isLessMaxKeyLeng(keyWord) && isOnlyChars(keyWord) && isOkMinKeyLeng(keyWord)) { vld = true }
+      def isNoDupLet(keyWord: String) = if (keyWord.toList == keyWord.toList.distinct) true else false
+      if (isLessMaxKeyLeng(keyWord) &&
+        isOnlyChars(keyWord) &&
+        isOkMinKeyLeng(keyWord) &&
+        isNoDupLet(keyWord)) { vld = true; validateKey = keyWord.replace("j", "i") }
     } while (!vld)
+
   }
 
   def validInputChoice() {
