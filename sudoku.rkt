0ab25421d2347f243d5fe9a3a21de984ca916f58
usoghisa <usoghisa@hotmail.com>
2015-03-15 11:54:14 +0000

    Update sudoku.rkt
    trnsform matrix

--- sudoku.rkt
+++ sudoku.rkt
@@ -6,7 +6,7 @@
 
 (define mtrx '(
               (1 2 3 4 5 6 7 8 9) 
-              (3 0 3 0 5 0 7 8 9)
+              (2 0 3 4 5 0 7 8 9)
               (3 2 3 0 5 6 7 8 3)
               (4 2 3 4 5 6 7 8 9)
               (5 2 3 4 5 6 7 8 9)
@@ -27,6 +27,7 @@
 ;return single element parameterised
 (define (pivot mtrx r c n )
   (car(parts mtrx r c n)))
+
 ;return any block parameterised
 (define(anyBlock mtrx r1 c1 n1 r2 c2 n2 r3 c3 n3)
                (flatten(list
@@ -54,18 +55,18 @@
 (print "rtn #t #f is num  ret f " ) (number? (pivot mtrx 0 0 1 ))
 (print "is 0  " )(= 0 (pivot mtrx 1 0 1 ))
 
-(define (row a b c )
+(define (row a b c mtrx )
   (anyBlock mtrx 
           a 0 3 
           b 3 3 
           c 6 3 ))
 
-(row 0 0 0)
+(row 0 0 0 mtrx)
 
 (range 10)
 
-(print "row " ) (row 0 0 0)
-(print "row " ) (row 1 1 1)
+(print "row " ) (row 0 0 0 mtrx )
+(print "row " ) (row 1 1 1 mtrx)
 (define col1 (anyColum mtrx 
           0 0 1 
           1 0 1 
@@ -77,8 +78,8 @@
           7 0 1 
           8 0 1))
 #|MULTYLINE COMMENT|#
-(print "col1 " ) col1
-(print "row1 " )(row 0 0 0) 
+(print "col1 " )col1
+(print "row " )(row 0 0 0 mtrx) 
 (print "singlr ELE r2 c8 n1 ")(car(parts mtrx 2 8 1))
 (print "rtn set 6 4 5 r c n ")(car(parts mtrx 6 4 5)) ;r c n
 
@@ -89,77 +90,39 @@
          ((> 0 n))
          (else n)))row1))
 
+(define (changeToSet row)
+(map(lambda(n)
+        (cond ((= n 0) (set 1 2 3 4 5 6 7 8 9))
+         ((> n 0)(set n) ))
+    )row))
+
 (define mtrxSet(list 
- (changeZero (row 0 0 0))
- (changeZero (row 1 1 1))
- (changeZero (row 2 2 2))
- (changeZero (row 3 3 3)) 
- (changeZero (row 4 4 4))
- (changeZero (row 5 5 5)) 
- (changeZero (row 6 6 6))
- (changeZero (row 7 7 7))
- (changeZero (row 8 8 8))))
-(display  "matrix with 0 substitute with set \n" ) 
+ (changeToSet (row 0 0 8 mtrx))
+ (changeToSet (row 1 1 8 mtrx))
+ (changeToSet (row 2 2 8 mtrx))
+ (changeToSet (row 3 3 8 mtrx))
+ (changeToSet (row 4 4 8 mtrx))
+ (changeToSet (row 5 5 8 mtrx))
+ (changeToSet (row 6 6 8 mtrx))
+ (changeToSet (row 7 7 8 mtrx))
+ (changeToSet (row 8 8 8 mtrx))
+))
+
+(display  "^matrix with n substitute with set is mtrxSet \n" ) 
 mtrxSet
-
 (print "call pivot rtn set " ) (pivot mtrxSet 1 1 1 )
+(print "remove n 9 from set " )(set-remove (pivot mtrxSet 1 1 1 ) 9 )
+(print "remove n 4 from set2 " )(set-remove (set 1 3 4 ) (set-first (set 4)) )
+(print "remove (set 2) from set " )(set-remove (pivot mtrxSet 1 1 1 ) (set-first(pivot mtrxSet 1 0 1 )) )
 
-(print "remove n 2 from set " )(set-remove (pivot mtrxSet 1 1 1 ) 2 )
+(define (trasform mtrxSet r) 
+  (set! mtrxSet(list
+                r
+                (set-remove(pivot mtrxSet 1 1 1 )(set-first(pivot mtrxSet 1 0 1 )))))
+  (print "x" )mtrxSet )
 
-(display  "remove x = 3 from set \n" )
-#||#
-(let*((x(pivot mtrxSet 1 0 1 )))
-  (set-remove (pivot mtrxSet 1 1 1 ) x ))
-(let* (
-       (mS mtrxSet)
-       (x (pivot mS 1 0 1 ))
-       )
-  (display  "matrix with set in let* \n" )
-  (set-remove (pivot mS 1 1 1 ) x )
- mS )
+(trasform mtrxSet (row 0 0 8 mtrxSet))
 
-(let*((x (pivot mtrxSet 1 0 1 )))
-  (display  "matrix with set in let*2 \n" )
-  (set-remove (pivot mtrxSet 1 1 1 ) x )
-  mtrxSet)
-
-
-
-
-
-
-
-
-
-#|HELPING FUNC__________
-
-(let* (
-       (x 3)
-(y (+ x 1))
-)
-(print "+x y")(+ x y))
-
-;;func
-(define (double x)
-(* 2 x))
-; conditional 
-(define (my-max x y)
-(if (> x y) x y))
-(define (sign n)
-   (cond ((> n 0) 1)
-         ((< n 0) -1)
-         (else 0)))(sign 10)
-;fac
-(define (std-factorial n)
-  (if (zero? n)
-      1
-      (* n (std-factorial (- n 1)))))
-; tail rec
-(define (factorial n)
-(acc-factorial n 1))
-(define (acc-factorial n sofar)
-  
-(if (zero? n)
-sofar
-(acc-factorial (- n 1) (* sofar n))))
-|#
+(print "set-first") (set-first (pivot mtrxSet 1 0 1 ));(set-rest(pivot mtrxSet 1 0 1 )) 
+(print "set-count-element")(set-count(pivot mtrxSet 1 1 1 ))
+;http://stackoverflow.com/questions/7910714/modify-part-of-list-using-set
