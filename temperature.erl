f7dd2ac1430f7527560ca093ad577da456adc7c3
usoghisa <usoghisa@hotmail.com>
2015-03-22 18:08:50 +0000

    Update temperature.erl
    v2

--- temperature.erl
+++ temperature.erl
@@ -8,15 +8,13 @@
 temperatureConverter() ->
  receive
 	 {convertToFarrenheith, C} ->
-		ResC = 32+C*9/5,
-	 % io:format("~p C is ~p F~n", [C, 32+C*9/5]),
-	 displayTemperature(ResC),
-	 temperatureConverter();
-	 
-	 {convertToCelsious, F} ->
-		ResF = (F-32)*5/9,
-	 % io:format("~p F is ~p C~n", [F, (F-32)*5/9]),
+		ResF = 32+C*9/5,
 	 displayTemperature(ResF),
+	 temperatureConverter();
+	
+	 {convertToCelsious, F} ->
+		ResC = (F-32)*5/9,
+	 displayTemperature(ResC),
 	 temperatureConverter();
 	 
 	 {stop} ->
@@ -27,15 +25,15 @@ temperatureConverter() ->
  end.
 
 displayTemperature(Res) ->
- 	io:format("Print : ~p~n", [Res]).
+ 	io:format("The temperature is converted to  : ~p~n", [Res]).
 
 %% ====================================================================
-%% P1 = spawn(person, init, ["aaa"]),
+%% 
 %% ====================================================================
 % 
-% c(temperature).
-% temperAct = spawn(fun temperature:temperatureConverter/0).
-% temperAct = spawn(temperature,temperatureConverter,[]).
-% temperAct ! {convertToFarrenheith, 32}.
-% temperAct ! {convertToCelsious, 100}.
+% compile 	> c(temperature).
+% start process > TemperAct = spawn(fun temperature:temperatureConverter/0).
+% send msg > TemperAct ! {convertToFarrenheith, 32}.
+% send msg > TemperAct ! {convertToCelsious, 100}.
+
 % 
